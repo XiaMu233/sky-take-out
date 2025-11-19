d301bd60485c8d23b4a91d211cf14abe6b5c6961
XiaMu233 <1635276877@qq.com>
2025-11-19 23:50:52 +0800

    新增员工_代码完善(录入的用户名存在时,处理抛出的异常)

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -20,8 +20,8 @@ public interface EmployeeMapper {
      * 插入员工数据
      * @param employee
      */
-    @Insert("insert into employee(name, username, password, phone, sex, id_number, create_time, update_time, create_user, update_user, status) " +
-            "values " +
-            "(#{name},#{username},#{password},#{phone},#{sex},#{idNumber}#{createTime},#{updateTime}#{createUser},#{updateUser}#{status})")
+    @Insert("insert into employee(name, username, password, phone, sex, id_number, status, create_time, update_time, create_user, update_user) " +
+            "VALUES (#{name},#{username},#{password},#{phone},#{sex},#{idNumber},#{status},#{createTime},#{updateTime},#{createUser},#{updateUser})")
     void insert(Employee employee);
+
 }
